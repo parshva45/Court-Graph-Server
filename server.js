81201199f6f7a8a2e44be231e9015e944321b508
parshva45 <parshva45@gmail.com>
2018-08-21 01:12:44 +0530

    changing server.js for deploying

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express')
 var app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/assignment', {useNewUrlParser: true } );
+//mongoose.connect('mongodb://localhost/assignment', {useNewUrlParser: true } );
+mongoose.connect('mongodb://heroku_tq322vml:k9pufnn9n2876feaotg9u90qjq@ds125912.mlab.com:25912/heroku_tq322vml',
+  {useNewUrlParser: true });
 
 app.use(function(req, res, next) {
+
+  var allowedOrigins = [
+    "http://localhost:4200",
+    "https://court-angular-app.herokuapp.com"
+  ];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   res.header("Access-Control-Allow-Origin",
     "http://localhost:4200");
   res.header("Access-Control-Allow-Headers",
@@ -14,7 +26,11 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.get('/', function (req, res) {
+  res.send('Node.js app deployed successfully!')
+});
+
 var courtService = require('./services/court.service.server');
 courtService(app);
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
