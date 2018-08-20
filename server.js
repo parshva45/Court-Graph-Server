bcdafc5a6a258e6879a33097d7d0f8b3da441572
parshva45 <parshva45@gmail.com>
2018-08-21 02:31:56 +0530

    updating server.js

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
 
-  res.header("Access-Control-Allow-Origin",
-    "http://localhost:4200");
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods",
